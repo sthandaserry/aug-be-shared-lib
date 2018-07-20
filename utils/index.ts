6a22350b87d576bd4371227c154ba80efc178b8e
Kalimuthu Selvaraj <kalimuthu.selvaraj@goaugment.io>
2018-07-20 20:26:05 +0530

    Created mail config file.

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,4 @@
 export * from './encrypter';
 export * from './envelope';
 export * from './util';
+export * from './mailerconfig';
---
+++ utils/mailerconfig.ts
@@ -0,0 +1,7 @@
+export const mailerconfig = {
+    transport: 'smtps://test@gmail.com:testpass@smtp.gmail.com',
+    defaults: {
+        from: '"nest-modules" <modules@nestjs.com>',
+    },
+    templateDir: 'template url here!',
+};
