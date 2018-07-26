55a53fbb7da6318dd8d51812434c7e632bb416fb
Kalimuthu Selvaraj <kalimuthu.selvaraj@goaugment.io>
2018-07-26 17:19:57 +0530

    Completed user contest api endpoints.

--- modules/user-auth/user-auth.service.ts
+++ modules/user-auth/user-auth.service.ts
@@ -92,7 +92,7 @@ export class UserAuthService {
   }
 
   async validateUser(payload: UserJwtPayload): Promise<any> {
-    return await this.userAuthModel.findOne({ uname: payload.email });
+    return await this.userAuthModel.findOne({ email: payload.email });
   }
 
   async find(whereColumn): Promise<User[]> {
