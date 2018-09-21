8a139a91e4b5970db167b2251cae5d3928bb959e
Kalimuthu Selvaraj <kalimuthu.selvaraj@goaugment.io>
2018-09-21 21:26:12 +0530

    Modified paginate logic

--- decorators/pagination.decorator.ts
+++ decorators/pagination.decorator.ts
@@ -7,25 +7,32 @@ export class PaginationOptions {
 }
 
 export const Pagination = createParamDecorator((_: string, req: Request): PaginationOptions => {
-    req.query.limit = (typeof req.query.limit === 'number') ?
-        req.query.limit :
-        parseInt(req.query.limit, 10) || parseInt(process.env.SIZE, 10);
+    if (req.query.limit && req.query.skip) {
+        req.query.limit = (typeof req.query.limit === 'number') ?
+            req.query.limit :
+            parseInt(req.query.limit, 10) || parseInt(process.env.SIZE, 10);
 
-    req.query.skip = (typeof req.query.skip === 'number') ?
-        req.query.skip :
-        parseInt(req.query.skip, 10) || 0;
+        req.query.skip = (typeof req.query.skip === 'number') ?
+            req.query.skip :
+            parseInt(req.query.skip, 10) || 0;
 
-    if (req.query.limit > parseInt(process.env.MAX, 10)) {
-        req.query.limit = parseInt(process.env.MAX, 10);
-    } else if (req.query.limit < parseInt(process.env.MIN, 10)) {
-        req.query.limit = parseInt(process.env.MIN, 10);
+        if (req.query.limit > parseInt(process.env.MAX, 10)) {
+            req.query.limit = parseInt(process.env.MAX, 10);
+        } else if (req.query.limit < parseInt(process.env.MIN, 10)) {
+            req.query.limit = parseInt(process.env.MIN, 10);
+        }
+
+        const limit: number = req.query.limit;
+        const skip: number = req.query.skip;
+
+        return {
+            limit,
+            skip,
+        };
+    } else {
+        return {
+            limit : 0,
+            skip : 0,
+        };
     }
-
-    const limit: number = req.query.limit;
-    const skip: number = req.query.skip;
-
-    return {
-        limit,
-        skip,
-    };
 });
