a4af773474813284ab23a7243e884511ac008a8f
Kalimuthu Selvaraj <kalimuthu.selvaraj@goaugment.io>
2018-10-11 00:14:30 +0530

    Implemented changes.

--- modules/admin-auth/auth.module.ts
+++ modules/admin-auth/auth.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './jwt.strategy';
+import { DatabaseModule } from '../database/database.module';
 import { AuthController } from './auth.controller';
 import { ForgotpasswordController } from './forgotpassword.controller';
 import { ResetpasswordController } from './resetpassword.controller';
@@ -8,6 +9,7 @@ import { ResetpasswordController } from './resetpassword.controller';
 import { authProviders } from './auth.providers';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [AuthController, ForgotpasswordController, ResetpasswordController],
   providers: [AuthService, JwtStrategy, ...authProviders],
 })
--- modules/database/database.providers.ts
+++ modules/database/database.providers.ts
@@ -3,13 +3,6 @@ import * as mongoose from 'mongoose';
 export const databaseProviders = [
   {
     provide: 'DbConnectionToken',
-    useFactory: async (): Promise<typeof mongoose> => {
-      if (process.env.NODE_ENV === 'prod') {
-        return await mongoose.connect(process.env.DB);
-      }
-      if (process.env.NODE_ENV === 'dev') {
-        return await mongoose.connect(process.env.DEVDB);
-      }
-    },
+    useFactory: async (): Promise<typeof mongoose> => await mongoose.connect(process.env.DB),
   },
 ];
